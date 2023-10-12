f647b47b4cd59aa65d59b05c6003c48027366e10
HusseinnHM <husseinnhm7@gmail.com>
2023-10-13 01:49:14 +0300

    Add check providers methods

--- Neptunee.EntityFrameworkCore.MultiLanguage/Extensions/Helper.cs
+++ Neptunee.EntityFrameworkCore.MultiLanguage/Extensions/Helper.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
 namespace Neptunee.EntityFrameworkCore.MultiLanguage.Extensions;
 
 internal static class Helper
 {
-    internal static string FunctionName(string nameOfMethod) => ("multiLan" + nameOfMethod).ToLower();
+    internal static string FunctionName(string nameOfMethod) => ("MultiLan" + nameOfMethod).ToLower();
+    
+    internal static bool IsPostgreSqlProvider(this DatabaseFacade database)
+    {
+        return database.ProviderName?.Equals("Npgsql.EntityFrameworkCore.PostgreSQL", StringComparison.OrdinalIgnoreCase) ?? false;
+    }
+    internal static bool IsSqlServerProvider(this DatabaseFacade database)
+    {
+        return database.ProviderName?.Equals("Microsoft.EntityFrameworkCore.SqlServer", StringComparison.OrdinalIgnoreCase) ?? false;
+    }
 
 }
