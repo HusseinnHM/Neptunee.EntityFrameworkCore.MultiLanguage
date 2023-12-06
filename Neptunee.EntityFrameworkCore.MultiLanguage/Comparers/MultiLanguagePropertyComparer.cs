6e3c57cd78eda3680c4a4b91fd809de689a4ad68
HusseinnHM <husseinnhm7@gmail.com>
2023-12-06 13:49:25 +0300

    feat: add MultiLanguagePropertyComparer.

---
+++ Neptunee.EntityFrameworkCore.MultiLanguage/Comparers/MultiLanguagePropertyComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Neptunee.EntityFrameworkCore.MultiLanguage.Types;
+
+namespace Neptunee.EntityFrameworkCore.MultiLanguage.Comparers;
+
+public class MultiLanguagePropertyComparer : ValueComparer<MultiLanguageProperty>
+{
+    public MultiLanguagePropertyComparer() : base(
+        (l, r) => JsonSerializer.Serialize<MultiLanguageProperty>(l, new JsonSerializerOptions()) == JsonSerializer.Serialize<MultiLanguageProperty>(r, new JsonSerializerOptions()),
+        v => v.GetHashCode(),
+        v => JsonSerializer.Deserialize<MultiLanguageProperty>(JsonSerializer.Serialize(v, new JsonSerializerOptions()), new JsonSerializerOptions()!)!)
+    {
+    }
+}
